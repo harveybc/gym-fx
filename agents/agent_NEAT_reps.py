0fec92eb7176092f9a746a4053bf2173d2a59d50
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-05-07 01:39:35 -0500

    test
    test

--- agents/agent_NEAT_reps.py
+++ agents/agent_NEAT_reps.py
@@ -182,7 +182,7 @@ def run():
     rep = neat.Checkpointer(25, 900)
     pop.add_reporter(rep)
 
-    gen_best=pop.population[0]
+    gen_best=pop.population['1']
 
     # Run until the winner from a generation is able to solve the environment
     # or the user interrupts the process.
