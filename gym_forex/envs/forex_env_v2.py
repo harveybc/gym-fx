d4056b551612d4b801103e84accd6a0a256cf79e
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-06-10 02:33:15 -0500

    test
    test

--- gym_forex/envs/forex_env_v2.py
+++ gym_forex/envs/forex_env_v2.py
@@ -132,7 +132,7 @@ class ForexEnv2(gym.Env):
         # action space = discrete(nop,buy,sell),box(volume, take_profit, stop_loss)
         self.action_space = spaces.Tuple([
             spaces.Discrete(3), spaces.Discrete(3),  # nop, buy, sell
-            #spaces.Box(low=float(-1.0), high=float(1.0), shape=3, dtype=np.float32), # vol,tp,sl
+            spaces.Box(low=float(-1.0), high=float(1.0), shape=(3,), dtype=np.float32), # vol,tp,sl
         ])
         # observation_space=(16 columns + 3 state variables)* obs_ticks, shape=(width,height, channels?)
         self.observation_space = spaces.Box(low=float(-1.0), high=float(1.0), shape=(self.obs_ticks, 1, 19), dtype=np.float32)
