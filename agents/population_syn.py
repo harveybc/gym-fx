0c75f735bc47ace2a393380887a518147cc38f88
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-06-25 17:36:52 -0500

    test
    test

--- agents/population_syn.py
+++ agents/population_syn.py
@@ -36,10 +36,10 @@ class PopulationSyn(Population):
         
         for g in self.population.items():
             print('\nmin_fitness=',min_fitness,'\n')
-            print('\ng.key = ',g[1].key)
-            print('\ng.fitness=',g[1].fitness)
-            if g[1].fitness < min_fitness:
-                min_fitness = g[1].fitness
+            print('\ng.key = ',g[1].Key)
+            print('\ng.fitness=',g[1].Fitness)
+            if g[1].Fitness < min_fitness:
+                min_fitness = g[1].Fitness
                 less_fit = deepcopy(g)
         return less_fit                
 
