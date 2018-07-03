d95cbb2a4678ba02d4918c01773f89301a0475a3
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-07-03 02:01:42 -0500

    test
    test

--- agents/agent_DDQN_2.py
+++ agents/agent_DDQN_2.py
@@ -213,9 +213,9 @@ if __name__ == "__main__":
             #print("e:{}/{},t:{},p:{},e:{:.2}-".format(e, EPISODES, time, points,agent.epsilon))
             if done:
                 agent.update_target_model()
-                print("Done:Ep{}/{} Bal={}, r:{} , best:{}, last:{}".format(e, EPISODES, info["balance"],points, best_performance ,last_best_episode))
+                print("Done:Ep{}/{} Bal={}, points:{}, points_max:{} , best:{}, last:{}".format(e, EPISODES, info["balance"],points,points_max, best_performance ,last_best_episode))
                 # if performance decreased, loads the last optimum
-                if (points<points_max):
+                if (points<=points_max):
                     agent.restore_max()
                     print("max restored")
                     
@@ -226,7 +226,6 @@ if __name__ == "__main__":
                 break
                 #print("Done: Episodes{}/{} Balance={:.2}, reward: {:.7}, points: {} epsilon:{:.2}  ,".format(e, EPISODES, points,agent.epsilon))
                 
-
             if (len(agent.memory) > batch_size) and (time > state_size) and (time%REPLAYFACTOR==0) and (not done):
                 agent.replay(batch_size)
                 progress = info["tick_count"]*100/1450
