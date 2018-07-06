58e44b5f2b38e9a125b4d9c7a98e1f3c7704be54
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-07-06 04:15:43 -0500

    test
    test

--- agents/agent_NEAT.py
+++ agents/agent_NEAT.py
@@ -60,7 +60,7 @@ def run():
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
                          config_path)
     # uses the extended NEAT population PopulationSyn that synchronizes with singularity
-    pop = Population(config)
+    pop = neat.Population(config)
     # add reporters
     stats = neat.StatisticsReporter()
     pop.add_reporter(stats)
