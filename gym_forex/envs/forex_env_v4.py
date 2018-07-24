cc4abcb59fdb3c2f0a550d773b8c5dd0382ca1a4
Harvey Bastidas <harveybc@users.noreply.github.com>
2018-07-24 00:11:41 -0500

    test
    test

--- gym_forex/envs/forex_env_v4.py
+++ gym_forex/envs/forex_env_v4.py
@@ -353,9 +353,9 @@ class ForexEnv4(gym.Env):
             if self.c_c == 1:
                 reward = -(5.0 * self.initial_capital)
             # penaliza red que no hace nada
-            #if self.tick_count >= (self.num_ticks - 2):
-                #if self.num_closes < self.min_orders:
-                #    reward = -(0.5*self.initial_capital * (1-(self.num_closes/self.min_orders)))
+            if self.tick_count >= (self.num_ticks - 2):
+                if self.num_closes < self.min_orders:
+                    reward = -(self.initial_capital * (1 - (self.num_closes / self.min_orders)))
                 #if self.equity == self.initial_capital:
                 #    reward = -(10.0 * self.initial_capital)
                     
