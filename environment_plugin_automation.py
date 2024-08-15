58ea343aa3b50024b54040178bc765e0fed754dc
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-14 23:50:19 -0500

    Update environment_plugin_automation.py

--- environment_plugin_automation.py
+++ environment_plugin_automation.py
@@ -342,7 +342,7 @@ class AutomationEnv(gym.Env):
             # Penalize complexity for avoiding overfitting Kormogorov complexity (constant for all steps)
             reward_kormogorov = -self.kolmogorov_c/(sqr_max_steps)
             #  reward a large number of orders
-            reward_orders = (self.num_closes/(3000*self.max_steps))            
+            reward_orders = (self.num_closes/(300*self.max_steps))            
             # penalty cost
             penalty_cost = -1/sqr_max_steps # Normalize the reward
         #    if (self.order_status == 0) and (self.c_c==4) and (self.profit_pips>0): #Normal close for profit
