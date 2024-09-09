b8a0c8e5767efbebbc47fe2648fd36b582200cc1
Harvey Bastidas <harveybc@ingeni-us.com>
2024-09-09 17:21:35 -0500

    Update environment_plugin_automation.py

--- environment_plugin_automation.py
+++ environment_plugin_automation.py
@@ -352,7 +352,7 @@ class AutomationEnv(gym.Env):
         #set the lambda values (just for showing, please verify the actual values in optimizer)
         profit_lambda = 30.0    # Reward for profit
         orders_lambda = 0.1    # Reward for closing orders
-        complexity_lambda = 0.0001  # Complexity penalty strength (best overfitting with 0.1)
+        complexity_lambda = 0.001  # Complexity penalty strength (best overfitting with 0.1)
         l2_lambda = 0.7  # Regularization strength (best overfitting with 1)
         margin_call_lambda = 50 # Reward for margin call
         reward_auc_lambda = 1.0 # Reward for balance increase
