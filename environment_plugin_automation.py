dafc4cfe4c27ba2b609248477379675032b5125d
Harvey Bastidas <harveybc@ingeni-us.com>
2024-09-23 00:09:26 -0500

    Update environment_plugin_automation.py

--- environment_plugin_automation.py
+++ environment_plugin_automation.py
@@ -447,6 +447,9 @@ class AutomationEnv(gym.Env):
                     # profit, good behavior
                     else:
                         self.fitness = final_reward + profit_factor + (sharpe_ratio*sharpe_ratio)*math.sqrt(num_orders)
+            # avoid single orders
+            if num_orders < 3:
+                fitness = fitness/(3-num_orders)
 
 
             print(f"[ENV] genome_id: {genome_id}, balance: {self.balance}, n_ord: {len(self.orders_list)}, final_reward ({final_reward}) + sharpe_ratio ({sharpe_ratio}) = Fitness: {self.fitness}")
