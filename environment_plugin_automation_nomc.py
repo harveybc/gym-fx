c44f2f7c7b7eff47e230b20dec60239fac2a4986
Harvey Bastidas <harveybc@users.noreply.github.com>
2024-10-01 18:41:28 -0500

    Update environment_plugin_automation_nomc.py

--- environment_plugin_automation_nomc.py
+++ environment_plugin_automation_nomc.py
@@ -411,7 +411,7 @@ class AutomationEnv(gym.Env):
                     if profit_factor > 0 and sharpe_ratio > 0:
                         #sqrt_orders = math.sqrt(num_orders)
                         # best so far: self.fitness = abs(profit_factor)*sharpe_ratio
-                        self.fitness = num_orders*abs(profit_factor)*sharpe_ratio
+                        self.fitness = num_orders*num_orders*abs(profit_factor)*sharpe_ratio
                     else:
                         # best so far: self.fitness = abs(profit_factor)*sharpe_ratio
                         self.fitness = abs(profit_factor)*sharpe_ratio
